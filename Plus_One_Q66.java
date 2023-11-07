de05f8b212b8da1414432e2e193e600630316fcd
Prem192002 <premsingh192002@gmail.com>
2023-11-08 04:16:31 +0530

    second commit

--- Plus_One_Q66.java
+++ Plus_One_Q66.java
@@ -1,27 +1,33 @@
 public class Plus_One_Q66 {
     public static int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                
-                digits[i]++;
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]=digits[i]+1;
                 return digits;
-            } else {
-                
-                digits[i] = 0;
             }
+            digits[i]=0;
+            
+            
+            
         }
-        
-        
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-        return result;
-    }
 
-    public static void main(String[] args) {
-        int[] num = {1, 2, 3};
-        int[] answer = plusOne(num);
-        for (int i = 0; i < answer.length; i++) {
-            System.out.print(answer[i]);
+        digits=new int[n+1];
+        digits[0]=1;
+        return digits;
+            
         }
-    }
+
+
+    
+
+    public static void main(String[] args){
+        int[] num = {9,9,9};
+        int[] answer = plusOne(num);
+        for(int i=0;i<answer.length;i++){
+        System.out.print(answer[i]+" ");
+        }
+    
+    
+}
 }
