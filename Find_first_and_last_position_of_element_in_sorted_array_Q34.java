25f0d2d48881da6c38fc6f1c27fb9ba60f4af141
Prem192002 <premsingh192002@gmail.com>
2023-11-20 17:28:57 +0530

    first commit

---
+++ Find_first_and_last_position_of_element_in_sorted_array_Q34.java
@@ -0,0 +1,50 @@
+public class Find_first_and_last_position_of_element_in_sorted_array_Q34{
+    public int[] searchRange(int[] nums, int target) {
+        int start = findStart(nums, target);
+        
+        // If target is not found, return [-1, -1]
+        if (start == -1 || nums[start] != target) {
+            return new int[]{-1, -1};
+        }
+        
+        int end = findEnd(nums, target);
+        
+        return new int[]{start, end};
+    }
+    
+    private int findStart(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        int result = -1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (nums[mid] >= target) {
+                end = mid - 1;
+                result = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return result;
+    }
+    
+    private int findEnd(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        int result = -1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (nums[mid] <= target) {
+                start = mid + 1;
+                result = mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return result;
+    }
+}
