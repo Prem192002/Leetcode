bc630b0305f762b61aedb7122f988c444d57ad7d
Prem192002 <premsingh192002@gmail.com>
2023-11-30 23:01:44 +0530

    first commit

---
+++ Assign_cookies_Q455.java
@@ -0,0 +1,21 @@
+public class Assign_cookies_Q455 {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int contentChildren = 0;
+        int i = 0; 
+        int j = 0; 
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+
+                contentChildren++;
+                i++; 
+            }
+            j++;
+        }
+
+        return contentChildren;
+    }
+}
