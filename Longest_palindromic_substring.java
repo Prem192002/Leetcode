9316ee7d4a89bf38d1a121d2e0d7471b5325f7e6
Prem192002 <premsingh192002@gmail.com>
2023-12-31 23:44:07 +0530

    first commit

---
+++ Longest_palindromic_substring.java
@@ -0,0 +1,36 @@
+public class Longest_palindromic_substring {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+
+        int n = s.length();
+        int start = 0, maxLen = 1;
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLen = 2;
+            }
+        }
+        for (int length = 3; length <= n; length++) {
+            for (int i = 0; i <= n - length; i++) {
+                int j = i + length - 1;
+                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = true;
+                    start = i;
+                    maxLen = length;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+    public static void main(String[] args){
+
+    }
+}
