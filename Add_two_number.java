79787f1c63b1692cc4e21fcfda0c7a05ac599b71
Prem192002 <premsingh192002@gmail.com>
2024-01-20 23:32:38 +0530

    first commit

---
+++ Add_two_number.java
@@ -0,0 +1,67 @@
+public class Add_two_number {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        // Example 1
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        Add_two_number  solution = new Add_two_number ();
+        ListNode result1 = solution.addTwoNumbers(l1, l2);
+        printListNode(result1);
+
+        // Example 2
+        ListNode l3 = new ListNode(9, new ListNode(9, new ListNode(9)));
+        ListNode l4 = new ListNode(1);
+
+        ListNode result2 = solution.addTwoNumbers(l3, l4);
+        printListNode(result2);
+    }
+
+    public static void printListNode(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // Define the ListNode class
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
