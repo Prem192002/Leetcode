acaa551a244007fe9054ce888d47ad1a76eb052f
Prem192002 <premsingh192002@gmail.com>
2024-01-27 23:29:10 +0530

    first commit

---
+++ Merge_two_sorted_list.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class Merge_two_sorted_list {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+        if (list1 != null) {
+            current.next = list1;
+        } else if (list2 != null) {
+            current.next = list2;
+        }
+        return dummy.next;
+    }
+    public static void main(String args[]) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        Merge_two_sorted_list merger = new Merge_two_sorted_list();
+        ListNode mergedList = merger.mergeTwoLists(list1, list2);
+
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+}
