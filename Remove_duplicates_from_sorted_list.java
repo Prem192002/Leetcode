40ef31af321c26860e1e3588cc66fbd92c2bba28
Prem192002 <premsingh192002@gmail.com>
2024-02-11 00:00:23 +0530

    first

---
+++ Remove_duplicates_from_sorted_list.java
@@ -0,0 +1,18 @@
+public class Remove_duplicates_from_sorted_list {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
